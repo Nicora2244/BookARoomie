c7aeacd73562ea8f33953ddb5aa47a5ffab0504b
camilalopez09 <camilalovevas@gmail.com>
2023-11-23 16:36:47 -0500

    Sin Modal

--- src/components/DatesErrorModal.jsx
+++ src/components/DatesErrorModal.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import {makeStyles} from '@m';
-import Modal from '@material-ui/core/Modal';
-import Backdrop from '@material-ui/core/Backdrop';
-import Fade from '@material-ui/core/Fade';
-import base from '../../tailwind.config';
+/*import React from 'react';
+/*import {makeStyles} from '@m';
 
-const useStyles= makeStyles((theme) => ({
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade'; */
+
+
+
+
+
+
+/*const useStyles= makeStyles((theme) => ({
 	modal: {
 		display: 'flex',
 		alignItems: 'center',
@@ -70,4 +74,4 @@ function DatesErrorModal({ handleClose, open }) {
 		</>
 	);
 }
-export default DatesErrorModal;
+export default DatesErrorModal;*/
